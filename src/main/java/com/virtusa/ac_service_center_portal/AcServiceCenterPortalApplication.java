11862f07b15e6d7407012bff32d07c8a7774a5c3
Bibek Chakraborty <bibekbibek0612@gmail.com>
2023-05-07 00:34:38 +0530

    springboot setup

---
+++ src/main/java/com/virtusa/ac_service_center_portal/AcServiceCenterPortalApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.ac_service_center_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcServiceCenterPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcServiceCenterPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/virtusa/ac_service_center_portal/AcServiceCenterPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.ac_service_center_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcServiceCenterPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
